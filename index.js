95451c70dfbcb4dc65688d3cba2ff965fa5e9947
Tomoko-hub <haisaitomoco@gmail.com>
2024-08-05 21:07:45 +0300

    metropolia node.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./db/dbconfig');
+const query = require('./db/customers');
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get('/api/customers', query.getAllCustomers);
+app.get('/api/customers/:id', query.getCustomerById);
+app.post('/api/customers', query.addCustomer);
+app.delete('/api/customers/:id', query.deleteCustomer);
+app.delete('/api/customers', query.deleteAllCustomers);
+app.put('/api/customers/:id', query.updateCustomer);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
